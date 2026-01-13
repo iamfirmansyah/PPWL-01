5909c4dd9b24e37bd74efa8a2965b25afe63a136
Imam Firmansyah <imam.firmansyah@yesdok.com>
2026-01-14 05:11:47 +0700

    Fix route names - change category to categories for consistency

--- routes/web.php
+++ routes/web.php
@@ -22,8 +22,8 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::resource('/category', CategoryController::class);
-    Route::resource('/products', ProductController::class);
+    Route::resource('categories', CategoryController::class);
+    Route::resource('products', ProductController::class);
 
     // KERANJANG PESANAN
     Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
